f35408dc5e240c18651c8aa2b9e56729c0366090
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2023-05-01 12:25:22 -0400

    initial commit

---
+++ R/imputez.R
@@ -0,0 +1,59 @@
+
+#' Impute z-score based correlations
+#' 
+#' Impute z-score from a missing test based on z-scores for other tests, and the correlation matrix between z-scores
+#' 
+#' @param z vector of observed z-scores
+#' @param Sigma matrix of correlation between z-scores
+#' @param i index of z-scores to impute
+#' @param lambda value used to shrink correlation matrix
+#' 
+#' @return \code{data.frame} with variant \code{ID}, imputed \code{z.stat} for test \code{i}, standard error \code{se} of the impute z-score, and metric of accuracy \code{r2.pred}.
+#' 
+#' @importFrom Matrix Diagonal
+#' @export
+impute_z = function(z, Sigma, i, lambda = 0.1){
+	if( is(Sigma, "sparseMatrix") ){
+		Sigma.shrink = (1-lambda) * Sigma + Diagonal(nrow(Sigma), lambda)
+	}else{
+		Sigma.shrink = (1-lambda) * Sigma + diag(lambda, nrow(Sigma))
+	}
+	# impute the ith z-score using 
+	# impute z-score using Gaussian conditional distribution
+	# z_i = Sigma.shrink[i,-i] %*% solve(Sigma.shrink[-i,-i], z[-i])
+	# W = Sigma.shrink[i,-i] %*% solve(Sigma.shrink[-i,-i])
+	# weights
+	W = solve(Sigma.shrink[-i,-i], Sigma.shrink[-i,i,drop=FALSE])
+
+	# imputed z-scores
+	z_i = crossprod(W, z[-i])
+
+	# compute standard error for each imputed z-score
+	sig = Sigma.shrink[i,i] - (crossprod(W,Sigma.shrink[-i, -i]) %*% W)
+	
+	data.frame(ID = names(z)[i], 
+						z.stat = as.numeric(z_i), 
+						se = as.numeric(sig),
+						r2.pred = 1 - as.numeric(sig))
+}
+
+
+
+# Instead do reverse diagonal
+@export
+pairwiseCompleteWindow = function(S, mid){
+	# Reduce window until most distance SNPs have LD computed
+	a = 1
+	b = ncol(S)
+	alterate = TRUE
+
+	while(S[a,b] == 0){
+		if( alterate )
+			a <- min(a + 1, mid)
+		else
+			b <- max(b - 1, mid)
+		alterate = ! alterate
+	}
+
+	c(a,b)
+}
